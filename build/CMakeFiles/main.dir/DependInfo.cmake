d47170b13489536d625d22880a4813428e14c4ff
usuari <aniol.sala.95@gmail.com>
2019-09-22 19:00:09 +0200

    Clean code

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/usuari/Programming/Other_things/C/projects/partial_diff_equations/SchrEquation/1D/functions.c" "/home/usuari/Programming/Other_things/C/projects/partial_diff_equations/SchrEquation/1D/build/CMakeFiles/main.dir/functions.c.o"
+  "/home/usuari/Programming/Other_things/C/projects/partial_diff_equations/SchrEquation/1D/main.c" "/home/usuari/Programming/Other_things/C/projects/partial_diff_equations/SchrEquation/1D/build/CMakeFiles/main.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
